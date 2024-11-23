5931203744389416389280d021a79c869ba6d377
Sulaiman <sulaimanallahham83@gmail.com>
2024-11-23 12:30:03 +0300

    fixed auth issue in forum page

--- resources/views/forums/forum.blade.php
+++ resources/views/forums/forum.blade.php
@@ -3,8 +3,9 @@
         <div class="d-flex mb-2 justify-content-between">
             <span class="fs-4">{{ $forum->postsLast7Days() . ' ' . __('common.posts_plural_ar') }} <span
                     class="fs-6 text-secondary">{{ __('forums.last_days', ['days' => 7]) }}</span></span>
-            <a href="{{ getLocaleURL('/forums/' . $forum->id . '/create') }}" class="btn btn-aabu rounded-pill px-4">Create
-                Post</a>
+            @auth
+                <a href="{{ getLocaleURL('/forums/' . $forum->id . '/create') }}" class="btn btn-aabu rounded-pill px-4">Create Post</a>
+            @endauth
         </div>
         <div id="forums-container">
             {{-- all forum posts go here --}}
